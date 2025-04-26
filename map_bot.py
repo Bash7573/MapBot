a07d99875f95dc865ef45bda2cef69f05cdf34e8
Bash7573 <jrlblake17@outlook.com>
2025-04-26 13:37:58 +1000

    Migrated DB to neww schema, added constraints, redid some functions, added new functions.

--- map_bot.py
+++ map_bot.py
@@ -3,11 +3,15 @@ import discord
 import datetime
 import time
 import os
+import re
 
 from dotenv import load_dotenv
 from datetime import datetime
 from discord.ext import commands
 from dataclasses import dataclass
+
+from sqlalchemy import create_engine, Column, Integer, Text, DateTime, String, CheckConstraint, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 # IMPORTANT CONSTATNTS
 
@@ -26,37 +30,15 @@ class MapNameData:
     """
     Stores the name of the maps
     """
-
-    map_names = ["antarctic-peninsula",
-                 "busan",
-                 "ilios",
-                 "lijang-tower",
-                 "nepal",
-                 "oasis",
-                 "samoa",
-                 "circuit-royale",
-                 "dorado",
-                 "havana",
-                 "junkertown", 
-                 "rialto",
-                 "route-66",
-                 "shambali-monastery",
-                 "watchpoint-gibraltar",
-                 "new-junk-city",
-                 "suravasa",
-                 "blizzard-world",
-                 "eichenwalde",
-                 "hollywood",
-                 "kings-row",
-                 "midtown",
-                 "numbani",
-                 "paraiso",
-                 "colosseo",
-                 "esperanca",
-                 "new-queen-street",
-                 "runasapi",
-                 "hanaoka",
-                 "throne-of-anubis"]
+    map_names = ["antarctic-peninsula","busan","ilios",
+                 "lijang-tower","nepal","oasis",
+                 "samoa","circuit-royale","dorado",
+                 "havana","junkertown", "rialto",
+                 "route-66","shambali-monastery","watchpoint-gibraltar",
+                 "new-junk-city","suravasa","blizzard-world",
+                 "eichenwalde","hollywood","kings-row","midtown",
+                 "numbani","paraiso","colosseo",
+                 "esperanca","new-queen-street","runasapi",]
     
 
 @dataclass
@@ -64,20 +46,103 @@ class BotData:
     """
     General Data the bot keeps tracks of in a session.
     """
-    bot_commands = ["madd [map-name] [result]",
-                    "mwinrate [map-map]",
-                    "mlastwon [map-name]",
-                    "mlastplayed [map_name]",
+    bot_commands = ["madd map_name result --stack --season",
+                    "mwinrate season map_name",
+                    "mlastwon map_name",
+                    "mlastplayed map_name",
                     "mlast10",
-                    "mbestmaps",
-                    "mworstmaps",
+                    "mseasonbestmaps",
+                    "mseasonworstmaps",
+                    "mseasonbestmaps",
+                    "mseasonworstmaps",
                     "mmostplayed",
+                    "mmostplayedall",
                     "mmaps"]   
 
 
-# Instatiate data classes
+Base = declarative_base()
+
+class MapReference(Base):
+    __tablename__ = 'map_ref'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    map_name = Column(String, unique=True, nullable=False)
+    map_type = Column(String, nullable=False)
+
+
+class OwMapsTable(Base):
+    __tablename__ = 'owmaps'
+
+    # SCHEMA
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    season = Column(Text, nullable=False)
+    map_name = Column(Text, ForeignKey("map_ref.map_name"), nullable=False)
+    map_result = Column(Text, nullable=False)
+    stack = Column(Text, nullable=True)
+    timestamp = Column(DateTime, nullable=False)
+    added_by = Column(Text, nullable=False)
+
+    # CONSTRAINTS
+    __table_args__ = (
+        CheckConstraint("season LIKE 's__' AND CAST(SUBSTR(season, 2, 2) AS INTEGER) BETWEEN 0 and 99", name = 'check_season_format'),
+        CheckConstraint("map_name IN ('antarctic-peninsula','busan','ilios','lijang-tower',"
+                        "'nepal','oasis','samoa','circuit-royale','dorado','havana','junkertown',"
+                        "'rialto','route-66','shambali-monastery','watchpoint-gibraltar','new-junk-city',"
+                        "'suravasa','blizzard-world','eichenwalde','hollywood','kings-row','midtown',"
+                        "'numbani','paraiso','colosseo','esperanca','new-queen-street','runasapi')", 
+                        name ='check_map_name'),
+
+        CheckConstraint("map_result IN ('w', 'l', 'd')", name = 'check_map_result'),
+        CheckConstraint("LENGTH(stack) <= 5", name="check_stack_length"),        
+        )
+        
+
+engine = create_engine(f'sqlite:///{DB_PATH}')
+Base.metadata.create_all(engine)
+
 bot_data = BotData()
 map_name_data = MapNameData()
+
+
+# Populate ref table
+
+connection = sqlite3.connect(DB_PATH)
+cursor = connection.cursor()
+
+cursor.execute("SELECT COUNT(*) FROM map_ref")
+count = cursor.fetchone()[0]
+
+if count == 0:
+    maps = [("antarctic-peninsula", "control"), ("busan", "control"),
+            ("ilios", "control"), ("lijang-tower", "control"),
+            ("nepal", "control"), ("oasis", "control"),
+            ("samoa", "control"), ("circuit-royale", "escort"),
+            ("dorado", "escort"), ("havana", "escort"),
+            ("junkertown", "escort"), ("rialto", "escort"),
+            ("route-66", "escort"), ("shambali-monastery", "escort"),
+            ("watchpoint-gibraltar", "escort"),("new-junk-city", "flashpoint"),
+            ("suravasa", "flashpoint"), ("blizzard-world", "hybrid"),
+            ("eichenwalde", "hybrid"), ("hollywood", "hybrid"),
+            ("kings-row", "hybrid"), ("midtown", "hybrid"),
+            ("numbani", "hybrid"), ("paraiso", "hybrid"),
+            ("colosseo", "push"), ("esperanca", "push"),
+            ("new-queen-street", "push"), ("runasapi", "push"),]
+
+    cursor.executemany("INSERT INTO map_ref (map_name, map_type) VALUES (?, ?)", maps)
+    connection.commit()
+    print("✅ Map reference table populated!")
+else:
+    print("ℹ️ Table already populated, skipping insert.")
+
+connection.close()
+
+# Helper Functions
+
+def validate_stack(stack):
+    allowed_chars = 'LWDEJC'
+    return all(char in allowed_chars for char in stack)
+
 
 # Bot Behaviour
 
@@ -95,26 +160,36 @@ async def on_ready():
 
 
 @bot.command()
-async def add(ctx, map_name: str, result: bool):
+async def add(ctx, map_name: str, map_result: str, season='s16', stack=None):
     """
-    Command to add map to the database
-    Arguments:
-        map_name: name of the map played
-        won: wwhether the map was one or not True or False
     """
     cur_timestamp = time.time()
     added_at = datetime.fromtimestamp(cur_timestamp).strftime("%Y-%m-%d %H:%M:%S")
-
-    #Connecting to database
+    
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
 
     try:
-        cursor.execute("INSERT INTO maps (map_name, added_by, timestamp, result) VALUES (?, ?, ?, ?)", (map_name, ctx.author.name, added_at, result))
+
+        if stack is not None:
+            check_stack = validate_stack(stack)
+            if stack is not None and not check_stack:
+                raise ValueError("Invalid stack")
+        
+        cursor.execute("INSERT INTO owmaps (season, map_name, map_result, stack, timestamp, added_by) VALUES (?, ?, ?, ?, ?, ?)", (season, map_name, map_result, stack, added_at, ctx.author.name))
         connection.commit()
-        await ctx.send(f"Map '{map_name}' added successfully by {ctx.author.name} @ {added_at}")
+        await ctx.send(f"✅ Map '{map_name}' added successfully by {ctx.author.name} @ {added_at}")
+
+    except ValueError as e:
+        await ctx.send(f"⚠️ ValueError: {e} (Invalid value)")
+    except sqlite3.IntegrityError as e:
+        await ctx.send(f"⚠️ Integrity Error: {e} (Invalid value or constraint failed)")
+    except sqlite3.OperationalError as e:
+        await ctx.send(f"⚠️ Operational Error: {e} (SQL syntax or connection issue)")
+    except sqlite3.DatabaseError as e:
+        await ctx.send(f"⚠️ Database Error: {e}")
     except Exception as e:
-        await ctx.send(f"An error occured: {e}")
+        await ctx.send(f"⚠️ Unexpected Error: {e}")
     finally:
         connection.close()
 
@@ -128,13 +203,15 @@ async def lastwon(ctx, map_name: str):
     curf_time = datetime.fromtimestamp(cur_time).strftime("%Y-%m-%d %H:%M:%S")
     cur_time_obj = datetime.strptime(curf_time, "%Y-%m-%d %H:%M:%S")
 
-    query = f"SELECT MAX(timestamp) FROM maps WHERE map_name = '{map_name}' AND result = 1"
+    query = f"SELECT MAX(timestamp), season FROM owmaps WHERE map_name = '{map_name}' AND map_result = 'w'"
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
 
     try:
         cursor.execute(query)
-        last_time = cursor.fetchone()[0]
+        result = cursor.fetchone()
+        last_time = result[0]
+        last_season = result[1]
 
         if last_time != None:
 
@@ -143,7 +220,7 @@ async def lastwon(ctx, map_name: str):
             days = elapsed_time.days
             hours, remainder = divmod(elapsed_time.seconds, 3600)
             minutes, seconds = divmod(remainder, 60)
-            await ctx.send(f"Time Elapsed since last win on {map_name}: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
+            await ctx.send(f"You last won {map_name} in {last_season} which was: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds ago")
         else:
             await ctx.send(f"No recorded win in database")
 
@@ -154,7 +231,32 @@ async def lastwon(ctx, map_name: str):
 
 
 @bot.command()
-async def winrate(ctx, map_name :str):
+async def personal_wr(ctx, name):
+    """
+    """
+    query = f"SELECT COUNT (*) FROM owmaps WHERE stack LIKE '%{name}%' AND map_result = 'w'"
+    query2 = f"SELECT COUNT (*) FROM owmaps WHERE stack LIKE '%{name}%'"
+
+    connection = sqlite3.connect(DB_PATH)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(query)
+        won = cursor.fetchone()
+        cursor.execute(query2)
+        total = cursor.fetchone()
+
+        wrate = round((int(won[0]) / int(total[0]) * 100), 2)
+        await ctx.send(f"{wrate}% - {total[0]} maps played")
+
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+    finally:
+        connection.close()
+
+
+@bot.command()
+async def winrate(ctx, season: str, map_name :str):
     """
     Fetches winrate for a given map
     returns as a percentage
@@ -162,8 +264,8 @@ async def winrate(ctx, map_name :str):
         map_name: the name of the map
     """
 
-    query = f"SELECT COUNT(*) FROM maps WHERE map_name = '{map_name}' AND result = 1"
-    query2 = f"SELECT COUNT(*) FROM maps WHERE map_name = '{map_name}'"
+    query = f"SELECT COUNT(*) FROM owmaps WHERE map_name = '{map_name}' AND map_result = 'w' AND season = '{season}'"
+    query2 = f"SELECT COUNT(*) FROM owmaps WHERE map_name = '{map_name}' AND season = '{season}'"
 
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
@@ -181,16 +283,84 @@ async def winrate(ctx, map_name :str):
         connection.close()
 
 @bot.command()
-async def bestmaps(ctx):
+async def seasonbestmaps(ctx, season):
     """
     Provides information on the maps with the best recordss
+    """
+    query = "SELECT map_name, COUNT(*) AS total_results," \
+    "SUM(CASE WHEN map_result = 'l' THEN 1 ELSE 0 END) AS total_losses," \
+    "SUM(CASE WHEN map_result = 'w' THEN 1 ELSE 0 END) AS total_wins," \
+    "SUM(CASE WHEN map_result = 'd' THEN 1 ElSE 0 END) AS total_draws," \
+    "(CAST(SUM(CASE WHEN map_result = 'w' THEN 1 ELSE 0 END) AS FLOAT) / COUNT(*) * 100) AS win_rate" \
+    f" FROM owmaps WHERE season = '{season}' GROUP BY map_name ORDER BY win_rate DESC, total_wins DESC"
+    connection = sqlite3.connect(DB_PATH)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        parsed_result = f"=== BEST MAPS - {season} ===\n"
+        
+        for i in result:
+            name = i[0].strip()
+            winrate = round(i[5], 2)
+            record = f"{i[3]}W {i[2]}L {i[4]}D"
+            parsed_result += name + " - " + str(winrate)+"%" + " - " + record + "\n"
+
+        await ctx.send(parsed_result)
+
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+    finally:
+        connection.close()
+
+
+@bot.command()
+async def seasonworstmaps(ctx, season):
+    """
+    Provides information on the maps with the worst records
     """
     query = "SELECT map_name, COUNT(*) AS total_results," \
     "SUM(CASE WHEN result = 0 THEN 1 ELSE 0 END) AS total_losses," \
     "SUM(CASE WHEN result = 1 THEN 1 ELSE 0 END) AS total_wins," \
     "SUM(CASE WHEN result = 2 THEN 1 ElSE 0 END) AS total_draws," \
     "(CAST(SUM(CASE WHEN result = 1 THEN 1 ELSE 0 END) AS FLOAT) / COUNT(*) * 100) AS win_rate" \
-    " FROM maps GROUP BY map_name ORDER BY win_rate DESC, total_wins DESC LIMIT 10"
+    f"FROM owmaps WWHRE season = '{season}' GROUP BY map_name ORDER BY win_rate ASC, total_losses DESC"
+    connection = sqlite3.connect(DB_PATH)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        parsed_result = f"=== WORST MAPS - {season} ===\n"
+        
+        for i in result:
+            name = i[0].strip()
+            winrate = round(i[5], 2)
+            record = f"{i[3]}W {i[2]}L {i[4]}D"
+            parsed_result += name + " - " + str(winrate)+"%" + " - " + record + "\n"
+
+        await ctx.send(parsed_result)
+
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+    finally:
+        connection.close()
+
+
+@bot.command()
+async def bestmaps(ctx):
+    """
+    Provides information on the maps with the best recordss
+    """
+    query = "SELECT map_name, COUNT(*) AS total_results," \
+    "SUM(CASE WHEN map_result = 'l' THEN 1 ELSE 0 END) AS total_losses," \
+    "SUM(CASE WHEN map_result = 'w' THEN 1 ELSE 0 END) AS total_wins," \
+    "SUM(CASE WHEN map_result = 'd' THEN 1 ElSE 0 END) AS total_draws," \
+    "(CAST(SUM(CASE WHEN map_result = 'w' THEN 1 ELSE 0 END) AS FLOAT) / COUNT(*) * 100) AS win_rate" \
+    f" FROM owmaps GROUP BY map_name ORDER BY win_rate DESC, total_wins DESC"
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
 
@@ -213,6 +383,7 @@ async def bestmaps(ctx):
     finally:
         connection.close()
 
+
 @bot.command()
 async def worstmaps(ctx):
     """
@@ -223,7 +394,7 @@ async def worstmaps(ctx):
     "SUM(CASE WHEN result = 1 THEN 1 ELSE 0 END) AS total_wins," \
     "SUM(CASE WHEN result = 2 THEN 1 ElSE 0 END) AS total_draws," \
     "(CAST(SUM(CASE WHEN result = 1 THEN 1 ELSE 0 END) AS FLOAT) / COUNT(*) * 100) AS win_rate" \
-    " FROM maps GROUP BY map_name ORDER BY win_rate ASC, total_losses DESC LIMIT 10"
+    " FROM maps GROUP BY map_name ORDER BY win_rate ASC, total_losses DESC"
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
 
@@ -252,7 +423,7 @@ async def last10 (ctx):
     """
     Last 10 Results
     """
-    query = f"SELECT map_name, result, timestamp FROM maps ORDER BY timestamp DESC LIMIT 10"
+    query = f"SELECT map_name, map_result, timestamp FROM owmaps ORDER BY timestamp DESC LIMIT 10"
     
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
@@ -265,12 +436,12 @@ async def last10 (ctx):
         for i in result:
             
             line = ""
-            if i[1] == 0:
+            if i[1] == 'l':
                 line += "❌ loss "
-            elif i[1] == 1:
+            elif i[1] == 'w':
                 line += "✅ win "
-            elif i[1] == 2:
-                line += "draw"
+            elif i[1] == 'd':
+                line += "⛔ draw"
 
             line += i[0].strip()
             line += f" {i[2]}"
@@ -286,18 +457,42 @@ async def last10 (ctx):
 
 
 @bot.command()
-async def mostplayed(ctx):
+async def mostplayed(ctx, season):
     """
     Shows most played maps
     """
-    query = f"SELECT map_name, count(*) FROM maps group by map_name order by count(*) DESC"
+    query = f"SELECT map_name, COUNT(*) FROM owmaps WHERE season = '{season}' GROUP BY map_name ORDER BY COUNT(*) DESC"
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
 
     try:
         cursor.execute(query)
         result = cursor.fetchall()
-        msg = "=== Most Played Maps\n"
+        msg = f"=== Most Played Maps - {season}\n"
+
+        for i in result:
+            msg += f"{i[0]}: {i[1]} times\n"
+
+        await ctx.send(msg)
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+    finally:
+        connection.close()
+
+
+@bot.command()
+async def mostplayedall(ctx):
+    """
+    Shows most played maps
+    """
+    query = f"SELECT map_name, COUNT(*) FROM owmaps GROUP BY map_name ORDER BY COUNT(*) DESC"
+    connection = sqlite3.connect(DB_PATH)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        msg = f"=== Most Played Maps - All Time\n"
 
         for i in result:
             msg += f"{i[0]}: {i[1]} times\n"
@@ -318,7 +513,7 @@ async def lastplayed(ctx, map_name):
     curf_time = datetime.fromtimestamp(cur_time).strftime("%Y-%m-%d %H:%M:%S")
     cur_time_obj = datetime.strptime(curf_time, "%Y-%m-%d %H:%M:%S")
 
-    query = f"SELECT MAX(timestamp) FROM maps WHERE map_name = '{map_name}'"
+    query = f"SELECT MAX(timestamp) FROM owmaps WHERE map_name = '{map_name}'"
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
 
@@ -337,25 +532,6 @@ async def lastplayed(ctx, map_name):
         else:
             await ctx.send("Have not played this map.")
 
-    except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
-    finally:
-        connection.close()
-
-
-@bot.command()
-async def notsure(ctx):
-    """
-    """
-    query = "SELECT * FROM maps WHERE strftime('%H:%M:%d', timestamp) BETWEEN '20:00:00' and '23:59:59' AND result = 1"
-    
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
-
-    try:
-        cursor.execute(query)
-        result = cursor.fetchone()
-        await ctx.send(result)
     except Exception as e:
         await ctx.send(f"An error occurred: {e}")
     finally:
